27c63e3937f131decb03889c0acc560e63fa4a44
user <Global@mail.ua>
2018-12-14 16:50:07 +0200

    base controllers/Main.php

--- vendor/core/controllers/Main.php
+++ vendor/core/controllers/Main.php
@@ -1,5 +1,16 @@
 <?php
+
 namespace core\controllers;
+
+use core\View;
+
 class Main {
-    
+
+    public $view;
+    public $model;
+
+    public function __construct() {
+	$this->view = new View();
+    }
+
 }
