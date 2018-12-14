cb121b03f0880de61ba304f34c36114815d444df
user <Global@mail.ua>
2018-12-14 17:01:42 +0200

    index method

--- vendor/controllers/Main.php
+++ vendor/controllers/Main.php
@@ -1,6 +1,20 @@
 <?php
 
 namespace controllers;
-class Main {
+
+use core\controllers\Main as baseMain;
+use models\Articles;
+
+class Main extends baseMain {
     
+    public function __construct() {
+	parent::__construct();
+	$this->model = new Articles();
+    }
+
+    public function index() {
+	$this->view->articles = $this->model->getAll();
+	$this->view->render();
+    }
+
 }
