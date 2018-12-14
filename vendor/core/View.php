1a931436baa8cee853b484648075d717760ee933
user <Global@mail.ua>
2018-12-14 17:10:29 +0200

    add page

--- vendor/core/View.php
+++ vendor/core/View.php
@@ -5,6 +5,7 @@ namespace core;
 class View {
 
     public $template = 'main';
+    public $page;
 
     public function render() {
 	include_once 'vendor/views/templates/' . $this->template . '.php';
