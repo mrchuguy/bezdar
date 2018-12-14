12d7ea59f8a8a119bf88587725205ee3f3b8bf29
user <Global@mail.ua>
2018-12-14 17:05:38 +0200

    Articlesmodel

--- vendor/models/Articles.php
+++ vendor/models/Articles.php
@@ -1,6 +1,29 @@
 <?php
 
 namespace models;
-class Articles {
-    //put your code here
+
+use core\models\Main;
+
+class Articles extends Main {
+
+    public function getAll() {
+	return array(
+	    array(
+		'title' => 'first',
+		'text' => 'many text',
+		'date' => '14.12.18'
+	    ),
+	    array(
+		'title' => 'second',
+		'text' => 'many text',
+		'date' => '17.12.18'
+	    ),
+	    array(
+		'title' => 'third',
+		'text' => 'many text',
+		'date' => '15.12.18'
+	    ),
+	);
+    }
+
 }
