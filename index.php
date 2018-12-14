30e6450e7848bdf8ed7af308c762f8a95bca4590
user <Global@mail.ua>
2018-12-14 16:39:07 +0200

    autoloader

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+spl_autoload_register(function($class){
+    $file = 'vendor'.DIRECTORY_SEPARATOR.$class.'.php';
+    if(file_exists($file)){
+	include_once $file;
+	return true;
+    }
+    return false;
+});
